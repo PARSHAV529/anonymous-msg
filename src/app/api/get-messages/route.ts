3a487b5acc944f0c994e18490340e946d5e8aba4
Parshav <parshav143@gmail.com>
2024-06-21 15:41:15 +0530

    frontend almost completeccommit

--- src/app/api/get-messages/route.ts
+++ src/app/api/get-messages/route.ts
@@ -22,63 +22,60 @@ export async function GET(req: Request) {
         })
     }
 
-    const userId = new mongoose.Types.ObjectId( user._id)
+    const userId = new mongoose.Types.ObjectId(user._id)
 
-try {
-    const AggregateUser = await userModel.aggregate([
-        {$match :{id: userId}},
-        {$unwind:'$messages'},
-        {$sort:{'messages.createAt':-1}},
-        {$group:{_id:'$_id',message:{$push:'messages'}}}
+    try {
+        const AggregateUser = await userModel.aggregate([
+            { $match: { id: userId } },
+            { $unwind: '$messages' },
+            { $sort: { 'messages.createAt': -1 } },
+            { $group: { _id: '$_id', message: { $push: 'messages' } } }
 
-    ])
+        ])
 
-    if(!AggregateUser || AggregateUser.length==0){
+        if (!AggregateUser || AggregateUser.length == 0) {
+
+            return Response.json({
+                success: false,
+                message: "user not found"
+
+            }, {
+                status: 404,
+            })
+        }
+        if (AggregateUser[0].length == 0)
+            {
+                return Response.json({
+                    success: false,
+                    messages: "message not found"
+    
+                }, {
+                    status: 404,
+                })
+            }
+
+         
 
         return Response.json({
-            success: false,
-            message: "user not found"
+            success: true,
+            messages: AggregateUser[0].messages
 
         }, {
-            status: 404,
+            status: 200,
         })
-    }
-    if(AggregateUser[0]. return Response.json({
-        success: true,
-        messages: AggregateUser[0].messages
-
-    }, {
-        status: 200,
-    }).length==0){
+    } catch (error) {
         return Response.json({
             success: false,
-            messages: "message not found"
-    
+            messages: "error while fetching user"
+
         }, {
-            status: 404,
+            status: 500,
         })
     }
 
-    return Response.json({
-        success: true,
-        messages: AggregateUser[0].messages
-
-    }, {
-        status: 200,
-    })
-} catch (error) {
-    return Response.json({
-        success: false,
-        messages: "error while fetching user"
-
-    }, {
-        status: 500,
-    })
-}
-   
 
 
 
 
-    
+
 }
